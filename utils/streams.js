2cd7d3df66a680bf350212ddf1f5a68ef6a81e66
aksana_tolstoguzova <oks1-92@mail.ru>
2017-10-12 22:16:59 -0700

    task3 - added generateTestCSS, fix writing from several streams in one file

--- utils/streams.js
+++ utils/streams.js
@@ -1,8 +1,6 @@
-#!/usr/bin/env node
-
-import { default as Argv } from 'yargs';
+import Argv from 'yargs';
 import fs from 'fs';
-import { default as through } from 'through2';
+import through from 'through2';
 import csvjson from 'csvjson';
 import path from 'path';
 import request from 'request';
@@ -14,7 +12,8 @@ const actions = {
   transform,
   csvToJsonStream,
   csvToJsonFile,
-  cssBundler
+  cssBundler,
+  generateTestCSS
 };
 
 export default actions;
@@ -36,7 +35,7 @@ function cliRun() {
     .option('action', {
       alias: 'a',
       describe: 'choose an action',
-      choices: ['io', 'transform', 'csvToJsonStream', 'csvToJsonFile', 'cssBundler']
+      choices: ['io', 'transform', 'csvToJsonStream', 'csvToJsonFile', 'cssBundler', 'generateTestCSS']
     })
     .option('file', {
       alias: 'f',
@@ -61,17 +60,35 @@ function inputOutput(filePath) { /* 4, babel-node ./utils/streams.js -a io -f ./
   else console.log('Please specify correct file path');
 }
 
+function stdInInformUser() {
+  process.stdout.write(`Please start typing to transform ...
+  to end input use CTRL + D for macOS and Alt-Space. Then, C for Windows\n`);
+  process.stdin.setEncoding('utf8');
+}
+
+function end() {
+  process.stdout.write('Transform was completed\n');
+}
+
+function write(buffer, encoding, next) {
+  this.push(`Transformed to: ${buffer.toString().toUpperCase()}`);
+  next();
+}
+
 function transform() { /* 5, node ./utils/streams.js -a transform */
   stdInInformUser();
-  
-  let write = function (buffer, encoding, next) {
-    this.push(`Transformed to: ${buffer.toString().toUpperCase()}`);
-    next();
-  };
-  
   process.stdin
     .pipe(through(write, end))
     .pipe(process.stdout);
+}
+
+function csvToJsonStreamTransform(buffer, encoding, next) {
+  let options = {
+    delimiter : ',',
+    quote     : '"'
+  };
+  this.push(JSON.stringify(csvjson.toObject(buffer.toString(), options)));
+  next();
 }
 
 function csvToJsonStream(filePath) { /* 6, babel-node ./utils/streams.js -a csvToJsonStream -f ./data/test.csv */
@@ -83,6 +100,14 @@ function csvToJsonStream(filePath) { /* 6, babel-node ./utils/streams.js -a csvT
       .pipe(process.stdout);
   }
   else console.log('Please specify correct file path');
+}
+
+function createFilePath(filePath, extension = '.json', name) {
+  const pathJsonFile = path.parse(filePath);
+  
+  pathJsonFile.ext = extension;
+  pathJsonFile.base = `${pathJsonFile.name || name}${pathJsonFile.ext}`;
+  return path.format(pathJsonFile);
 }
 
 function csvToJsonFile(filePath) { /* 7, babel-node ./utils/streams.js -a csvToJsonFile -f ./data/test.csv */
@@ -100,72 +125,85 @@ function csvToJsonFile(filePath) { /* 7, babel-node ./utils/streams.js -a csvToJ
   else console.log('Please specify correct file path');
 }
 
-function stdInInformUser() {
-  process.stdout.write(`Please start typing to transform ...
-  to end input use CTRL + D \n`);
-  process.stdin.setEncoding('utf8');
-}
-
-function csvToJsonStreamTransform(buffer, encoding, next) {
-  let options = {
-    delimiter : ',',
-    quote     : '"'
-  };
-  this.push(JSON.stringify(csvjson.toObject(buffer.toString(), options)));
-  next();
-}
-
-function end() {
-  process.stdout.write('Transform was completed\n');
-}
-
-function createFilePath(filePath, extension = '.json', name) {
-  const pathJsonFile = path.parse(filePath);
+function asyncOrderedWrite(cssFiles, writerableStream, pathFolder, additionalStream = () => {}) {
+  if (cssFiles.length === 0) {
+    additionalStream(writerableStream);
+    return;
+  }
+  let remainingCssFiles = cssFiles.splice(1);
+  let cssFile = cssFiles[0];
+  const readableStream = fs.createReadStream(`${pathFolder}${cssFile}`);
+  readableStream.setEncoding('utf8');
   
-  pathJsonFile.ext = extension;
-  pathJsonFile.base = `${pathJsonFile.name || name}${pathJsonFile.ext}`;
-  return path.format(pathJsonFile);
-}
-
-/* CSS Bundler */
-function cssBundler(path, resultFileName = 'bundle.css') { /* 8, babel-node -- ./utils/streams.js -a cssBundler -p ./data/css */
-  if (fs.existsSync(path)) {
-    const folderPath = getFolderPath(path);
-    const writerableStream = fs.createWriteStream(`${getFolderPath(folderPath)}${resultFileName}`);
-    combineCSSFiles(folderPath, resultFileName, writerableStream);
-    getCssFromWeb(BASE_CSS_URL, writerableStream);
-    console.log(`${resultFileName} was bundled`);
-  } else console.log('Please specify correct folder path');
-}
-
-function combineCSSFiles(pathFolder, resultFileName, writerableStream) {
-  fs.readdir(pathFolder, (err, files) => {
-    for (let i = 0; i < files.length; i++) {
-      if (path.extname(files[i]) === '.css' && files[i] !== resultFileName) {
-        let data = '';
-        const readableStream = fs.createReadStream(`${pathFolder}${files[i]}`);
-        readableStream.setEncoding('utf8');
-        
-        readableStream.on('data', function(chunk) {
-          data+=chunk;
-        });
-        
-        readableStream.on('end', function() {
-          i++;
-          writerableStream.write(data + "\n");
-        });
-      }
-    }
+  readableStream.on('data', function(chunk) {
+    writerableStream.write(chunk + "\n");
+  });
+  
+  readableStream.on('end', function() {
+    asyncOrderedWrite(remainingCssFiles, writerableStream, pathFolder, additionalStream);
   });
 }
 
-function getCssFromWeb(url, writerableStream) {
-  request(url).pipe(writerableStream);
+/* generate test files */
+function generateTestCSS(path = './data/test') {
+  fs.readdir(path, (err, files) => {
+    files.forEach(file => {
+      console.log(createFileName(`${getFolderPath(path)}${file}`, null, '_large'));
+      const writerableStream = fs.createWriteStream(createFileName(`${getFolderPath(path)}${file}`, null, '_large'));
+      let readableStream = fs.createReadStream(`${getFolderPath(path)}${file}`, 'utf8');
+      let data = '';
+      readableStream.on('data', function (chunk) {
+        data += chunk;
+      });
+      readableStream.on('end', function () {
+        writeDataToStream(data, writerableStream, 400000);
+      });
+    });
+  });
+}
+
+function writeDataToStream(data,writerableStream, count) {
+  if (count === 0) return;
+  writerableStream.write(data, function() {
+    let before = process.memoryUsage().rss;
+    writeDataToStream(data,writerableStream, --count);
+    console.log('memory increased by', Math.round((process.memoryUsage().rss - before) / 1024 / 1024), 'MB');
+  });
+}
+
+/* CSS Bundler */
+async function combineCSSFiles(pathFolder, resultFileName, writerableStream, additionalStream) {
+  fs.readdir(pathFolder, (err, files) => {
+    const cssFiles = files.filter(file => path.extname(file) === '.css' && file !== resultFileName);
+    asyncOrderedWrite(cssFiles, writerableStream, pathFolder, additionalStream);
+  });
+}
+
+function getCssFromWeb(writerableStream) {
+  request(BASE_CSS_URL).on('data', function(chunk) {
+    writerableStream.write(chunk);
+  });
 }
 
 function getFolderPath(path) {
   return path[path.length - 1] === "/" ? path : `${path}/`;
 }
 
+function createFileName(filePath, extension, prefixName) {
+  const pathJsonFile = path.parse(filePath);
+  if (extension) {
+    pathJsonFile.ext = extension;
+  }
+  pathJsonFile.base = `${pathJsonFile.name}${prefixName}${pathJsonFile.ext}`;
+  return path.format(pathJsonFile);
+}
 
-
+function cssBundler(path, resultFileName = 'bundle.css') { /* 8, babel-node -- ./utils/streams.js -a cssBundler -p ./data/css */
+  if (fs.existsSync(path)) {
+    const folderPath = getFolderPath(path);
+    const writerableStream = fs.createWriteStream(`${getFolderPath(folderPath)}${resultFileName}`);
+    combineCSSFiles(folderPath, resultFileName, writerableStream, getCssFromWeb);
+    console.log(`${resultFileName} was bundled`);
+    
+  } else console.log('Please specify correct folder path');
+}
