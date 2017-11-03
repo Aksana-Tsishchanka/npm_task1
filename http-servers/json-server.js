79ca30ee7c46bd4717753696403910969dade219
aksana_tolstoguzova <oks1-92@mail.ru>
2017-11-02 19:52:43 -0700

    task4 - middleware, express

--- http-servers/json-server.js
+++ http-servers/json-server.js
@@ -4,7 +4,8 @@ import http from 'http';
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const product = { id: 1,
+const product = {
+  id: 1,
   name: 'Supreme T-Shirt',
   brand: 'Supreme',
   price: 99.99,
